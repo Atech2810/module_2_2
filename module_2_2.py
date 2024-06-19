e19393d5ac2bfe7a0ea32510ab275b1b7fd05bc4
Atech2810 <isakova2810@gmail.com>
2024-06-19 14:10:55 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,24 @@
+first = 567
+second = 84
+third = 49
+print(first)
+print(second)
+print(third)
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+first = 897
+second = 897
+third = 8
+print(first)
+print(second)
+print(third)
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
